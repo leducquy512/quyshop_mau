ad8dcd300382fee9926426e6bc5b88a3264663d3
TanPhat310309 <dailyducky044@gmail.com>
2026-01-27 11:20:51 +0700

    Add multiple files

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Welcome from './Welcome';
+import Welcome from './welcome';
 
 function App() {
   return (
     <>
+    <div className="background">
       <div className="name">
         <Welcome name="Tấn Phát" />
       </div>
@@ -14,6 +15,7 @@ function App() {
       <div>
         <h2>Sở thích: Nghe nhạc, Anime/Manga</h2>
         <h2>Lớp: 26.SP.IT3</h2>
+      </div>
       </div>
     </>
   );
